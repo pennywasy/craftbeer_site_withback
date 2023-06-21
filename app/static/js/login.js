7ccfb38257f1d9d266ccd3cae70b14f2795b2bfc
Виталий Егоров <egorov@citbars.ru>
2023-06-21 09:01:22 +0400

    changes

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -16,10 +16,26 @@ $(document).ready(function(){
 	});
 
 	$("#form__registry").submit(function(e){
+		e.preventDefault();
+		let data = new Date($("#birthday").val());
+	    let now = new Date();
+	    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
+	    let datanow = new Date(today.getFullYear(), data.getMonth(), data.getDate());
+	    let age = today.getFullYear() - data.getFullYear();
+	    if(today < datanow){
+        	age -= 1;
+   		}
+
 		if ($('#password').val() != $('#re-password').val()){
 			e.preventDefault();
-			$('#password').css('background-color', '#ffa9a9');
-			$('#re-password').css('background-color', '#ffa9a9');
+			$('#password').attr('class', 'error');
+			$('#re-password').attr('class', 'error');
+		}
+
+		if(age < 18){
+			e.preventDefault();
+			$("#birthday").attr('class', 'error');
+
 		}
 
 	})
